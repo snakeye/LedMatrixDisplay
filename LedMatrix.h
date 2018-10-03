e93e7f2e7189933440eaf0c84098be212eee3128
Andrey Ovcharov <andrew.ovcharov@gmail.com>
2018-10-03 15:04:50 +0200

    Added code sample

--- LedMatrix.h
+++ LedMatrix.h
@@ -6,343 +6,377 @@
 #include "font.h"
 
 // max7219 registers
-#define MAX7219_REG_NOOP         0x0
-#define MAX7219_REG_DIGIT0       0x1
-#define MAX7219_REG_DIGIT1       0x2
-#define MAX7219_REG_DIGIT2       0x3
-#define MAX7219_REG_DIGIT3       0x4
-#define MAX7219_REG_DIGIT4       0x5
-#define MAX7219_REG_DIGIT5       0x6
-#define MAX7219_REG_DIGIT6       0x7
-#define MAX7219_REG_DIGIT7       0x8
-#define MAX7219_REG_DECODEMODE   0x9
-#define MAX7219_REG_INTENSITY    0xA
-#define MAX7219_REG_SCANLIMIT    0xB
-#define MAX7219_REG_SHUTDOWN     0xC
-#define MAX7219_REG_DISPLAYTEST  0xF
+#define MAX7219_REG_NOOP 0x0
+#define MAX7219_REG_DIGIT0 0x1
+#define MAX7219_REG_DIGIT1 0x2
+#define MAX7219_REG_DIGIT2 0x3
+#define MAX7219_REG_DIGIT3 0x4
+#define MAX7219_REG_DIGIT4 0x5
+#define MAX7219_REG_DIGIT5 0x6
+#define MAX7219_REG_DIGIT6 0x7
+#define MAX7219_REG_DIGIT7 0x8
+#define MAX7219_REG_DECODEMODE 0x9
+#define MAX7219_REG_INTENSITY 0xA
+#define MAX7219_REG_SCANLIMIT 0xB
+#define MAX7219_REG_SHUTDOWN 0xC
+#define MAX7219_REG_DISPLAYTEST 0xF
 
-#define TEXT_ALIGN_LEFT          0 // Text is aligned to left side of the display
-#define TEXT_ALIGN_LEFT_END      1 // Beginning of text is just outside the right end of the display
-#define TEXT_ALIGN_RIGHT         2 // End of text is aligned to the right of the display
-#define TEXT_ALIGN_RIGHT_END     3 // End of text is just outside the left side of the display
-#define TEXT_ALIGN_CENTER        4 // center
+#define TEXT_ALIGN_LEFT 0      // Text is aligned to left side of the display
+#define TEXT_ALIGN_LEFT_END 1  // Beginning of text is just outside the right end of the display
+#define TEXT_ALIGN_RIGHT 2     // End of text is aligned to the right of the display
+#define TEXT_ALIGN_RIGHT_END 3 // End of text is just outside the left side of the display
+#define TEXT_ALIGN_CENTER 4    // center
 
-namespace MAX7219 {
+namespace MAX7219
+{
 
 /**
    Template class to define pin assignment
 */
-template<const int CS_PIN>
-class PinAssignment {
-  public:
-    static const int pinCS = CS_PIN;
+template <const int CS_PIN>
+class PinAssignment
+{
+public:
+  static const int pinCS = CS_PIN;
 };
 
 /**
    Display driver
 */
-template<const int matrixCount, class PinAssignment>
-class DisplayDriver {
-  public:
-    // width of the display in pixels
-    const int width = matrixCount * 8;
-    // height of the display in pixel
-    const int height = 8;
+template <const int matrixCount, class PinAssignment>
+class DisplayDriver
+{
+public:
+  // width of the display in pixels
+  const int width = matrixCount * 8;
+  // height of the display in pixel
+  const int height = 8;
 
-  public:
-    /**
+public:
+  /**
       Initializes the SPI interface
 
       @scope: device driver
     */
-    void init() {
-      pinMode(PinAssignment::pinCS, OUTPUT);
+  void init()
+  {
+    pinMode(PinAssignment::pinCS, OUTPUT);
 
-      // start SPI
-      SPI.begin();
-      SPI.setDataMode(SPI_MODE0);
-      SPI.setClockDivider(SPI_CLOCK_DIV128);
+    // start SPI
+    SPI.begin();
+    SPI.setDataMode(SPI_MODE0);
+    SPI.setClockDivider(SPI_CLOCK_DIV128);
 
-      // initialize MAX7219 devices
-      for (int device = 0; device < matrixCount; device++) {
-        sendByte(device, MAX7219_REG_SCANLIMIT, 7);   // show all 8 digits
-        sendByte(device, MAX7219_REG_DECODEMODE, 0); // using an led matrix (not digits)
-        sendByte(device, MAX7219_REG_DISPLAYTEST, 0); // no display test
-        sendByte(device, MAX7219_REG_INTENSITY, 0); // character intensity: range: 0 to 15
-        sendByte(device, MAX7219_REG_SHUTDOWN, 1); // not in shutdown mode (ie. start it up)
-      }
+    // initialize MAX7219 devices
+    for (int device = 0; device < matrixCount; device++)
+    {
+      sendByte(device, MAX7219_REG_SCANLIMIT, 7);   // show all 8 digits
+      sendByte(device, MAX7219_REG_DECODEMODE, 0);  // using an led matrix (not digits)
+      sendByte(device, MAX7219_REG_DISPLAYTEST, 0); // no display test
+      sendByte(device, MAX7219_REG_INTENSITY, 0);   // character intensity: range: 0 to 15
+      sendByte(device, MAX7219_REG_SHUTDOWN, 1);    // not in shutdown mode (ie. start it up)
     }
+  }
 
-    /**
+  /**
       Send block of data to the displays
 
       @scope: device driver
     */
-    void sendBytes(const byte spiregister[matrixCount],	const byte spidata[matrixCount]) {
-      // enable the line
-      digitalWrite(PinAssignment::pinCS, LOW);
+  void sendBytes(const byte spiregister[matrixCount], const byte spidata[matrixCount])
+  {
+    // enable the line
+    digitalWrite(PinAssignment::pinCS, LOW);
 
-      // now shift out the data
-      for (int i = 0; i < matrixCount; i++) {
-        SPI.transfer(spiregister[matrixCount - i - 1]);
-        SPI.transfer(spidata[matrixCount - i - 1]);
-      }
-
-      digitalWrite(PinAssignment::pinCS, HIGH);
+    // now shift out the data
+    for (int i = 0; i < matrixCount; i++)
+    {
+      SPI.transfer(spiregister[matrixCount - i - 1]);
+      SPI.transfer(spidata[matrixCount - i - 1]);
     }
 
-    /**
+    digitalWrite(PinAssignment::pinCS, HIGH);
+  }
+
+  /**
       Send a byte to a specific device.
 
       @scope: device driver
     */
-    void sendByte(const byte device, const byte reg, const byte data) {
-      byte spidata[matrixCount] = { 0 };
-      byte spiregister[matrixCount] = { 0 };
+  void sendByte(const byte device, const byte reg, const byte data)
+  {
+    byte spidata[matrixCount] = {0};
+    byte spiregister[matrixCount] = {0};
 
-      // put our device data into the array
-      spiregister[device] = reg;
-      spidata[device] = data;
+    // put our device data into the array
+    spiregister[device] = reg;
+    spidata[device] = data;
 
-      sendBytes(spiregister, spidata);
-    }
+    sendBytes(spiregister, spidata);
+  }
 
-    /**
+  /**
       Send a byte to all devices (convenience method).
 
       @scope: device driver
     */
-    void sendByte(const byte reg, const byte data) {
-      byte spidata[matrixCount];
-      byte spiregister[matrixCount];
+  void sendByte(const byte reg, const byte data)
+  {
+    byte spidata[matrixCount] = {0};
+    byte spiregister[matrixCount] = {0};
 
-      for (int i = 0; i < matrixCount; i++) {
-        spidata[i] = data;
-        spiregister[i] = reg;
-      }
-
-      sendBytes(spiregister, spidata);
+    for (int i = 0; i < matrixCount; i++)
+    {
+      spidata[i] = data;
+      spiregister[i] = reg;
     }
+
+    sendBytes(spiregister, spidata);
+  }
 };
 
 /**
 
 */
-template<const int width, const int height>
-class FrameBuffer {
-  protected:
-    /**
+template <const int width, const int height>
+class FrameBuffer
+{
+protected:
+  /**
       Set a specific column with a byte value to the frame buffer.
 
       @scope: frame buffer
     */
-    void setColumn(int col, byte value) {
-      if (col < 0 || col >= width) {
-        return;
-      }
-
-      frameBuffer[col] = value;
+  void setColumn(int col, byte value)
+  {
+    if (col < 0 || col >= width)
+    {
+      return;
     }
 
-  protected:
-    byte frameBuffer[width] = { 0 };
+    frameBuffer[col] = value;
+  }
+
+protected:
+  byte frameBuffer[width] = {0};
 };
 
 /**
    Main display class
 */
-template<const int matrixCount, class PinAssignment>
-class LedMatrix: public DisplayDriver<matrixCount, PinAssignment>, public FrameBuffer<matrixCount * 8, 8> {
-  private:
-    typedef DisplayDriver<matrixCount, PinAssignment> Driver;
-    typedef FrameBuffer<matrixCount * 8, 8> TFrameBuffer;
+template <const int matrixCount, class PinAssignment>
+class LedMatrix : public DisplayDriver<matrixCount, PinAssignment>, public FrameBuffer<matrixCount * 8, 8>
+{
+private:
+  typedef DisplayDriver<matrixCount, PinAssignment> Driver;
+  typedef FrameBuffer<matrixCount * 8, 8> TFrameBuffer;
 
-  public:
-    /**
+public:
+  /**
       Set font for display
     */
-    void setFont(Font* newFont) {
-      font = newFont;
-    }
+  void setFont(Font *newFont)
+  {
+    font = newFont;
+  }
 
-    /**
+  /**
       Sets the intensity on all devices.
       intensity: 0-15
 
       @scope: display
     */
-    void setIntensity(int intensity) {
-      Driver::sendByte(MAX7219_REG_INTENSITY, intensity);
-    }
+  void setIntensity(int intensity)
+  {
+    Driver::sendByte(MAX7219_REG_INTENSITY, intensity);
+  }
 
-    /**
+  /**
       Clear the frame buffer.
 
       @scope: display
     */
-    void clear() {
-      for (int col = 0; col < matrixCount * 8; col++) {
-        TFrameBuffer::frameBuffer[col] = 0;
-      }
+  void clear()
+  {
+    for (int col = 0; col < matrixCount * 8; col++)
+    {
+      TFrameBuffer::frameBuffer[col] = 0;
     }
+  }
 
-    /**
+  /**
       Turn on pixel at position (x, y).
 
       @scope: display
     */
-    void setPixel(int x, int y) {
-      bitWrite(TFrameBuffer::frameBuffer[x], y, true);
-    }
+  void setPixel(int x, int y)
+  {
+    bitWrite(TFrameBuffer::frameBuffer[x], y, true);
+  }
 
-    /**
+  /**
       Get pixel value at position (x, y)
 
       @scope: display
     */
-    bool getPixel(int x, int y) {
-      return bitRead(TFrameBuffer::frameBuffer[x], y);
-    }
+  bool getPixel(int x, int y)
+  {
+    return bitRead(TFrameBuffer::frameBuffer[x], y);
+  }
 
-    /**
+  /**
 
     */
-    int drawChar(int x, int y, char ch) {
-      if (ch > 127 || font == NULL)
-        return 0;
+  int drawChar(int x, int y, char ch)
+  {
+    if (ch > 127 || font == NULL)
+      return 0;
 
-      byte mask = (y >= 0) ? (0xFF << y) : (0xFF > y);
+    byte mask = (y >= 0) ? (0xFF << y) : (0xFF > y);
 
-      // character width
-      int char_width = font->getCharWidth(ch);
-      if (char_width == 0)
-        return 0;
+    // character width
+    int char_width = font->getCharWidth(ch);
+    if (char_width == 0)
+      return 0;
 
-      // draw character
-      for (int j = 0; j < char_width; j++) {
-        int col = x + j;
+    // draw character
+    for (int j = 0; j < char_width; j++)
+    {
+      int col = x + j;
 
-        if (col >= 0 && col < (matrixCount * 8)) {
-          // read column pixels from program memory
-          byte pixels = font->getCharColumn(ch, j);
+      if (col >= 0 && col < (matrixCount * 8))
+      {
+        // read column pixels from program memory
+        byte pixels = font->getCharColumn(ch, j);
 
-          // shift pixels
-          pixels = (y >= 0) ? (pixels << y) : (pixels >> (-y));
+        // shift pixels
+        pixels = (y >= 0) ? (pixels << y) : (pixels >> (-y));
 
-          // update canvas
-          TFrameBuffer::frameBuffer[col] &= ~(mask);
-          TFrameBuffer::frameBuffer[col] |= pixels;
-        }
+        // update canvas
+        TFrameBuffer::frameBuffer[col] &= ~(mask);
+        TFrameBuffer::frameBuffer[col] |= pixels;
       }
-
-      return char_width;
     }
 
-    /**
+    return char_width;
+  }
+
+  /**
       Draw the current text at the current offset with the current font
     */
-    void drawText() {
-      if (font == NULL) {
-        return;
-      }
-
-      int x = myTextOffset + myTextAlignmentOffset;
-      int y = 0;
-
-      for (int i = 0; i < myText.length(); i++) {
-        char letter = myText.charAt(i);
-
-        int char_width = drawChar(x, y, letter);
-
-        x += char_width + 1;
-      }
+  void drawText()
+  {
+    if (font == NULL)
+    {
+      return;
     }
 
-    /**
+    int x = myTextOffset + myTextAlignmentOffset;
+    int y = 0;
+
+    for (int i = 0; i < myText.length(); i++)
+    {
+      char letter = myText.charAt(i);
+
+      int char_width = drawChar(x, y, letter);
+
+      x += char_width + 1;
+    }
+  }
+
+  /**
       Set the current text.
     */
-    void setText(String text) {
-      myText = text;
-      myTextOffset = 0;
-      calculateTextAlignmentOffset();
-    }
+  void setText(String text)
+  {
+    myText = text;
+    myTextOffset = 0;
+    calculateTextAlignmentOffset();
+  }
 
-    /**
+  /**
       Sets the text alignment.
       Default is TEXT_ALIGN_LEFT_END.
 
       @scope: display
     */
-    void setTextAlignment(int textAlignment) {
-      myTextAlignment = textAlignment;
-      calculateTextAlignmentOffset();
-    }
+  void setTextAlignment(int textAlignment)
+  {
+    myTextAlignment = textAlignment;
+    calculateTextAlignmentOffset();
+  }
 
-    /**
+  /**
       Scroll the text to the left.
     */
-    void scrollTextLeft() {
-      myTextOffset = (myTextOffset - 1)
-                     % (font->getStringWidth(myText) + matrixCount * 8);
-    }
+  void scrollTextLeft()
+  {
+    myTextOffset = (myTextOffset - 1) % (font->getStringWidth(myText) + matrixCount * 8);
+  }
 
-    /**
+  /**
       Writes the frame buffer to the displays.
     */
-    void commit() {
-      byte reg[matrixCount];
-      byte data[matrixCount];
+  void commit()
+  {
+    byte reg[matrixCount] = {0};
+    byte data[matrixCount] = {0};
 
-      for (int row = 0; row < 8; row++) {
-        for (int i = 0; i < matrixCount; i++) {
+    for (int row = 0; row < 8; row++)
+    {
+      for (int i = 0; i < matrixCount; i++)
+      {
 
-          // register value
-          reg[i] = row + 1;
+        // register value
+        reg[i] = row + 1;
 
-          // data value
-          for (int col = 0; col < 8; col++) {
-            byte pixel = getPixel(i * 8 + col, 8 - row - 1);
-            bitWrite(data[i], col, pixel);
-          }
+        // data value
+        for (int col = 0; col < 8; col++)
+        {
+          byte pixel = getPixel(i * 8 + col, 8 - row - 1);
+          bitWrite(data[i], col, pixel);
         }
-
-        Driver::sendBytes(reg, data);
       }
-    }
 
-  private:
-    /**
+      Driver::sendBytes(reg, data);
+    }
+  }
+
+private:
+  /**
       @scope: display
     */
-    void calculateTextAlignmentOffset() {
-      int stringWidth = font->getStringWidth(myText);
-      switch (myTextAlignment) {
-        case TEXT_ALIGN_LEFT:
-          myTextAlignmentOffset = 0;
-          break;
-        case TEXT_ALIGN_LEFT_END:
-          myTextAlignmentOffset = matrixCount * 8;
-          break;
-        case TEXT_ALIGN_RIGHT:
-          myTextAlignmentOffset = stringWidth - matrixCount * 8;
-          break;
-        case TEXT_ALIGN_RIGHT_END:
-          myTextAlignmentOffset = -stringWidth;
-          break;
-        case TEXT_ALIGN_CENTER:
-          myTextAlignmentOffset = (matrixCount * 8 - stringWidth) / 2;
-      }
+  void calculateTextAlignmentOffset()
+  {
+    int stringWidth = font->getStringWidth(myText);
+    switch (myTextAlignment)
+    {
+    case TEXT_ALIGN_LEFT:
+      myTextAlignmentOffset = 0;
+      break;
+    case TEXT_ALIGN_LEFT_END:
+      myTextAlignmentOffset = matrixCount * 8;
+      break;
+    case TEXT_ALIGN_RIGHT:
+      myTextAlignmentOffset = stringWidth - matrixCount * 8;
+      break;
+    case TEXT_ALIGN_RIGHT_END:
+      myTextAlignmentOffset = -stringWidth;
+      break;
+    case TEXT_ALIGN_CENTER:
+      myTextAlignmentOffset = (matrixCount * 8 - stringWidth) / 2;
     }
+  }
 
-  private:
-    Font* font = NULL;
+private:
+  Font *font = NULL;
 
-    String myText;
+  String myText;
 
-    int myTextOffset = 1;
-    int myTextAlignmentOffset = 0;
-    int increment = -1;
-    int myTextAlignment = 1;
+  int myTextOffset = 1;
+  int myTextAlignmentOffset = 0;
+  int increment = -1;
+  int myTextAlignment = 1;
 };
 
-}
+} // namespace MAX7219
--- README.md
+++ README.md
@@ -2,3 +2,35 @@
 MAX7219/MAX7221 library for ESP8266 and Arduino platform
 
 Based on https://github.com/squix78/MAX7219LedMatrix
+
+## Example code
+
+```cpp
+#include <SPI.h>
+#include "LedMatrix.h"
+
+#define NUMBER_OF_DEVICES 4
+#define CS_PIN 2
+
+typedef MAX7219::PinAssignment<CS_PIN> PinAssignment;
+typedef MAX7219::LedMatrix<NUMBER_OF_DEVICES, PinAssignment> LedMatrix;
+
+LedMatrix ledMatrix = LedMatrix();
+Font font = Font();
+
+void setup() {
+  Serial.begin(115200); // For debugging output
+  ledMatrix.init();
+  ledMatrix.setFont(&font);
+  ledMatrix.setIntensity(4); // range is 0-15
+  ledMatrix.setText("MAX7219 Demo");
+}
+
+void loop() {
+  ledMatrix.clear();
+  ledMatrix.scrollTextLeft();
+  ledMatrix.drawText();
+  ledMatrix.commit(); // commit transfers the byte buffer to the displays
+  delay(200);
+}
+```
