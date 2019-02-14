4cecf42db95f981e3999ba24ab524b2269200cac
Andrey Ovcharov <andrey.ovcharov@influencerdb.com>
2019-02-14 09:35:12 +0100

    register processing

--- include/arduino/LedMatrixDisplay_Arduino.h
+++ include/arduino/LedMatrixDisplay_Arduino.h
@@ -1,5 +1,7 @@
 #pragma once
 
+#include <SPI.h>
+
 #include "Driver.h"
 #include "MAX7219.h"
 
@@ -16,17 +18,15 @@ namespace MAX7219
 namespace Arduino
 {
 
-template <const unsigned int pinCS>
+template <const unsigned int _driverCount, const unsigned int pinCS>
 class MAX7219_SPI : public Driver
 {
 protected:
-  unsigned int driverCount;
+  const unsigned int driverCount = driverCount;
 
 public:
-  void init(const unsigned int _driverCount)
+  void init(const unsigned int _)
   {
-    driverCount = _driverCount;
-
     pinMode(pinCS, OUTPUT);
 
     SPI.begin();
@@ -36,23 +36,38 @@ public:
 
   void send(const unsigned char reg, const unsigned char value)
   {
+    unsigned char regs[driverCount];
+    unsigned char vals[driverCount];
 
+    for(unsigned int i = 0; i < driverCount; i++) {
+      regs[i] = reg;
+      vals[i] = value;
+    }
+
+    send(&regs[0], &vals[0]);
   }
 
   void send(const unsigned int matrix, const unsigned char reg, const unsigned char value)
   {
+    unsigned char regs[driverCount] = {0};
+    unsigned char vals[driverCount] = {0};
+
+    regs[matrix] = reg;
+    vals[matrix] = value;
+
+    send(&regs[0], &vals[0]);
   }
 
-  void send(const unsigned char *reg, const unsigned char *values)
+  void send(const unsigned char *regs, const unsigned char *values)
   {
     // enable the line
     digitalWrite(pinCS, LOW);
 
     // now shift out the data
-    for (int i = 0; i > driverCount; i--)
+    for (unsigned int i = 0; i < driverCount; i++)
     {
-      SPI.transfer(reg[driverCount - i]);
-      SPI.transfer(values[driverCount - i]);
+      SPI.transfer(regs[driverCount - i - 1]);
+      SPI.transfer(values[driverCount - i - 1]);
     }
 
     digitalWrite(pinCS, HIGH);
@@ -60,12 +75,12 @@ public:
 };
 
 template <class LedMatrixArray, const unsigned int pinCS>
-class LedMatrixDisplayColumnAnodeSPI : public LedMatrixDisplayColumnAnode<LedMatrixArray, MAX7219_SPI<pinCS>>
+class LedMatrixDisplayColumnAnodeSPI : public LedMatrixDisplayColumnAnode<LedMatrixArray, MAX7219_SPI<LedMatrixArray::matrixCount, pinCS>>
 {
 };
 
 template <class LedMatrixArray, const unsigned int pinCS>
-class LedMatrixDisplayColumnCathodeSPI : public LedMatrixDisplayColumnCathode<LedMatrixArray, MAX7219_SPI<pinCS>>
+class LedMatrixDisplayColumnCathodeSPI : public LedMatrixDisplayColumnCathode<LedMatrixArray, MAX7219_SPI<LedMatrixArray::matrixCount, pinCS>>
 {
 };
 
